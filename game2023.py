47486fd424ff9411b471e4b012d51554237e555b
p0wered <powered.ui@gmail.com>
2023-09-27 14:53:13 +0300

    1.1.3

--- game2023.py
+++ game2023.py
@@ -5,10 +5,10 @@ pg.init()
 
 screen_width, screen_height = 800, 600
 
-FPS = 24    # frame per second
+FPS = 60
 clock = pg.time.Clock()
 
-bg_img = pg.image.load('src/dz.png')
+bg_img = pg.image.load('src/background.png')
 icon_img = pg.image.load('src/ufo.png')
 
 display = pg.display.set_mode((screen_width, screen_height))
@@ -20,27 +20,37 @@ font = pg.font.Font('src/04B_19.TTF', 48)
 display.blit(bg_img, (0, 0))
 
 text_img = sys_font.render('Score 123', True, 'white')
-display.blit(text_img, (100, 50))
-
 game_over_text = font.render('Game Over', True, 'red')
 w, h = game_over_text.get_size()
-display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
 
 player_img = pg.image.load('src/player.png')
 player_width, player_height = player_img.get_size()
-
+player_gap = 10
+player_velocity = 10
+player_dx = 0
+player_x = screen_width/2 - player_width/2
+player_y = screen_height  - player_height - player_gap
 
 running = True
 while running:
+    player_x += player_dx
+    display.blit(bg_img, (0, 0))
+    display.blit(player_img, (player_x, player_y))
     pg.display.update()
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_q:
                 running = False
-            if event.key == pg.K_SPACE:
-                display.blit(bg_img, (0, 0))
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_a or event.key == pg.K_LEFT:
+                player_dx = -player_velocity
+            if event.key == pg.K_d or event.key == pg.K_RIGHT:
+                player_dx = player_velocity
+        if event.type == pg.KEYUP:
+            player_dx = 0
+
     clock.tick(FPS)
+
 pg.quit()
